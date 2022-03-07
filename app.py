16d82a7ea3e12f2e9a279ef5f8cc1fd814ad0e46
em4715 <em4715@naver.com>
2022-03-07 21:02:30 +0900

    first

---
+++ app.py
@@ -0,0 +1,53 @@
+from pymongo import MongoClient
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+client = MongoClient('mongodb+srv://test:sparta@Cluster0.d4msk.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+
+# HTML 화면 보여주기
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+@app.route('/battle_create')
+def battle_create():
+    return render_template('battle_create.html')
+
+@app.route('/battle_zone')
+def battle_zone():
+    return render_template('battle_zone.html')
+
+# API 역할을 하는 부분
+@app.route('/api/select1', methods=['POST'])
+def select_btn1():
+    title_receive = request.form['title_give']
+    target_title = db.battle.find_one({'title': title_receive})
+
+    current_sel1 = target_title['sel_cnt1']
+    new_sel1 = current_sel1+1
+
+    db.battle.update_one({'title': title_receive}, {'$set': {'sel_cnt1': new_sel1}})
+    return jsonify({'msg': '선택 완료'})
+
+@app.route('/api/select2', methods=['POST'])
+def select_btn2():
+    title_receive = request.form['title_give']
+    target_title = db.battle.find_one({'title': title_receive})
+
+    current_sel2 = target_title['sel_cnt2']
+    new_sel2 = current_sel2+1
+
+    db.battle.update_one({'title': title_receive}, {'$set': {'sel_cnt2': new_sel2}})
+    return jsonify({'msg': '선택 완료'})
+
+@app.route('/api/showbar', methods=['GET'])
+def show_bar():
+    bar = list(db.battle.find({'title': '깻잎 논쟁!'}, {'_id': False}))
+    return jsonify({'show_bars': bar})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
