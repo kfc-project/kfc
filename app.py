c8104774d005d228174b9c3c0a2e210d17399dee
nireggu <100943433+nireggu@users.noreply.github.com>
2022-03-07 22:01:45 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb+srv://test:sparta@Cluster0.d4msk.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
 
@@ -12,15 +12,15 @@ db = client.dbsparta
 def home():
     return render_template('main.html')
 
-@app.route('/battle_create')
+@app.route('/test')
 def battle_create():
-    return render_template('battle_create.html')
+    return render_template('test.html')
 
 @app.route('/battle_zone')
 def battle_zone():
     return render_template('battle_zone.html')
 
-# API 역할을 하는 부분
+# 왼쪽 '선택' 버튼을 눌렀을 시 작동
 @app.route('/api/select1', methods=['POST'])
 def select_btn1():
     title_receive = request.form['title_give']
@@ -32,6 +32,7 @@ def select_btn1():
     db.battle.update_one({'title': title_receive}, {'$set': {'sel_cnt1': new_sel1}})
     return jsonify({'msg': '선택 완료'})
 
+# 오른쪽 '선택' 버튼을 눌렀을 시 작동
 @app.route('/api/select2', methods=['POST'])
 def select_btn2():
     title_receive = request.form['title_give']
@@ -43,11 +44,35 @@ def select_btn2():
     db.battle.update_one({'title': title_receive}, {'$set': {'sel_cnt2': new_sel2}})
     return jsonify({'msg': '선택 완료'})
 
+# Progress Bar 표출
 @app.route('/api/showbar', methods=['GET'])
 def show_bar():
     bar = list(db.battle.find({'title': '깻잎 논쟁!'}, {'_id': False}))
     return jsonify({'show_bars': bar})
 
+# 토론장 생성하기
+@app.route('/api/sub_create', methods=['POST'])
+def sub_create():
+    title_receive = request.form['title_give']
+    sub1_receive = request.form['sub1_give']
+    sub2_receive = request.form['sub2_give']
+    img_receive = request.form['img_give']
+
+    target_title = db.battle.find_one({'title': title_receive})
+
+    current_seq = target_title['seq'];
+    new_seq = current_seq +1;
+
+    doc = {
+        'title': title_receive,
+        'subject1': sub1_receive,
+        'subject2': sub2_receive,
+        'img_src': img_receive,
+        'seq': new_seq
+    }
+    db.battle.insert_one(doc)
+
+    return jsonify({'msg': '생성되었습니다.'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
