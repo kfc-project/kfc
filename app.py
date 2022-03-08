e72c7056f44b3322d9766332534f335c00cbf53f
em4715 <em4715@naver.com>
2022-03-08 19:58:36 +0900

    prac_modified

--- app.py
+++ app.py
@@ -12,13 +12,16 @@ db = client.dbsparta
 def home():
     return render_template('main.html')
 
+
 @app.route('/battle_create')
 def battle_create():
     return render_template('battle_create.html')
 
+
 @app.route('/battle_zone')
 def battle_zone():
     return render_template('battle_zone.html')
+
 
 # 왼쪽 '선택' 버튼을 눌렀을 시 작동
 @app.route('/api/select1', methods=['POST'])
@@ -27,10 +30,11 @@ def select_btn1():
     target_title = db.battle.find_one({'title': title_receive})
 
     current_sel1 = target_title['sel_cnt1']
-    new_sel1 = current_sel1+1
+    new_sel1 = current_sel1 + 1
 
     db.battle.update_one({'title': title_receive}, {'$set': {'sel_cnt1': new_sel1}})
     return jsonify({'msg': '선택 완료'})
+
 
 # 오른쪽 '선택' 버튼을 눌렀을 시 작동
 @app.route('/api/select2', methods=['POST'])
@@ -39,16 +43,25 @@ def select_btn2():
     target_title = db.battle.find_one({'title': title_receive})
 
     current_sel2 = target_title['sel_cnt2']
-    new_sel2 = current_sel2+1
+    new_sel2 = current_sel2 + 1
 
     db.battle.update_one({'title': title_receive}, {'$set': {'sel_cnt2': new_sel2}})
     return jsonify({'msg': '선택 완료'})
+
 
 # Progress Bar 표출
 @app.route('/api/showbar', methods=['GET'])
 def show_bar():
     bar = list(db.battle.find({'title': '깻잎 논쟁!'}, {'_id': False}))
     return jsonify({'show_bars': bar})
+
+
+# main 토론장
+@app.route('/card', methods=['GET'])
+def card_get():
+    card_list = list(db.battle.find({}, {'_id': False}))
+    return jsonify({'cards': card_list})
+
 
 # 토론장 생성하기
 @app.route('/api/sub_create', methods=['POST'])
@@ -57,7 +70,6 @@ def sub_create():
     sub1_receive = request.form['sub1_give']
     sub2_receive = request.form['sub2_give']
     img_receive = request.form['img_give']
-
 
     doc = {
         'title': title_receive,
@@ -72,13 +84,15 @@ def sub_create():
 
     return jsonify({'msg': '생성되었습니다.'})
 
+
 # 토론장 이미지 업로드 하기
-@app.route('/uploader', methods=['GET','POST'])
+@app.route('/uploader', methods=['GET', 'POST'])
 def uploader_file():
-    if request.method == "POST" :
+    if request.method == "POST":
         file = request.files['file']
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
         return '이미지 업로드 성공'
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
