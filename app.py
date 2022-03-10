7bc91bd1790697a3ecf284937e59329e1fe3f3a3
goeb2013 <59667980+goeb2013@users.noreply.github.com>
2022-03-10 15:16:13 +0100

    하트 누르면 +1

--- app.py
+++ app.py
@@ -12,9 +12,33 @@ db = client.dbsparta
 def home():
     return render_template('main.html')
 
+@app.route('/card', methods=["POST"])
+def card_post():
+    title_receive = request.form['title_give']
+    img_receive = request.form['img_give']
+    link_receive = request.form['link_give']
+    num_receive = request.form['num_give']
+
+    doc = {
+        'title': title_receive,
+        'img': img_receive,
+        'link': link_receive,
+        'num': num_receive
+    }
+
+    db.cards.insert_one(doc)
+
+    return jsonify({'msg': '저장완료'})
+
+@app.route('/card', methods=["GET"])
+def card_get():
+    return jsonify({'msg': 'GET 연결 완료!'})
+
 @app.route('/battle_create')
 def battle_create():
-    return render_template('battle_create.html')
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': 'POST 연결 완료!'})
 
 @app.route('/battle_zone')
 def battle_zone():
@@ -43,11 +67,50 @@ def select_btn2():
     db.battle.update_one({'title': title_receive}, {'$set': {'sel_cnt2': new_sel2}})
     return jsonify({'msg': '선택 완료'})
 
+# 댓글 DB 보내기
+@app.route('/reply', methods=['POST'])
+def write_reply():
+    Id_receive = request.form['Id_give']
+    Password_receive = request.form['Password_give']
+    Comment_receive = request.form['Comment_give']
+
+    doc = {
+        'Id': Id_receive,
+        'Password': Password_receive,
+        'Comment': Comment_receive,
+        'like': 0
+    }
+
+    db.reply.insert_one(doc)
+
+    return jsonify({'msg': '댓글 저장 완료!'})
+
+# 댓글 DB 가져오기
+@app.route('/reply', methods=['GET'])
+def read_replies():
+    replies = list(db.reply.find({}, {'_id': False}))
+    return jsonify({'all_replies': replies})
+
+
 @app.route('/api/showbar', methods=['GET'])
 def show_bar():
     bar = list(db.battle.find({'title': '깻잎 논쟁!'}, {'_id': False}))
     return jsonify({'show_bars': bar})
 
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+
+# 좋아요 수 늘리기
+@app.route('/api/like', methods=['POST'])
+def like_comment():
+    Id_receive = request.form['Id_give']
+
+    target_id = db.reply.find_one({'Id': Id_receive})
+    current_like = target_id['like']
+
+    new_like = current_like + 1
+
+    db.reply.update_one({'Id': Id_receive}, {'$set': {'like': new_like}})
+
+    return jsonify({'msg': '좋아요 완료!'})
